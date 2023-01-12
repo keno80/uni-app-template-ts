e10c10c2d8851ec0855544b465b1d962b42f7530
ckkeno <ckkeno@gmail.com>
2023-01-12 11:27:17 +0800

    update: 更新uniapp版本，添加uni-ui

---
+++ src/pages/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="login">
+    <view class="login_content">
+      <view class="login_title">登录外卖跑腿端</view>
+      <uni-badge type="success" text="22"/>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
