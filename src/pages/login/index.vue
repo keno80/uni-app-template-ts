ed3371c48bf14bc512ff2233554a96ba999807d0
keno80 <1211582428@qq.com>
2023-02-03 17:22:18 +0800

    Update index.vue

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,7 +1,6 @@
 <template>
   <view class="login">
     <view class="login_content">
-      <view class="login_title">登录外卖跑腿端</view>
       <uni-badge type="success" text="22"/>
     </view>
   </view>
